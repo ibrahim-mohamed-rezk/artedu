f2fdfeb82f5e397909d365eaf10e3ed5aae73f56
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2025-03-18 01:43:06 +0200

    0

--- app/(app)/QuestionsBanck/page.tsx
+++ app/(app)/QuestionsBanck/page.tsx
@@ -1,14 +1,13 @@
-// import QuestionsBank from "@/components/questionsBank/QuestionsBank";
-// import QuestionsBankHeader from "@/components/questionsBank/QuestionsBankHeader";
+import QuestionsBank from "@/components/questionsBank/QuestionsBank";
+import QuestionsBankHeader from "@/components/questionsBank/QuestionsBankHeader";
 
 const page = () => {
   return (
     <div className="w-full">
-      {/* <QuestionsBankHeader />
+      <QuestionsBankHeader />
       <div className="container mx-auto mt-[180px] md:mt-0 ">
         <QuestionsBank />
-      </div> */}
-      hi
+      </div>
     </div>
   );
 };
