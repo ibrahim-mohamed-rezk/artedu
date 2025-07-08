c1fe58116e55482b9784c34236970ced22159aef
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2025-07-08 20:01:22 +0300

    Enhance support page with a new layout, including a heading and a link for account deletion instructions on Google Play.

--- app/(app)/support/page.tsx
+++ app/(app)/support/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Link from "next/link";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <h1>Support</h1>
+      <ul>
+        <li>
+          <Link href="/support/google-play-delete-account">
+            How to Delete Your Account (Google Play)
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default page
