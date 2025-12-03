7d62afe93b2d8ac5bde6425bf89940c9a23a25a7
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2025-12-03 12:31:08 +0200

    feat: implement personal settings management component with profile update functionality.

--- components/profile/account-information/PersonalSettings.tsx
+++ components/profile/account-information/PersonalSettings.tsx
@@ -495,7 +495,7 @@ const PersonalSettings = () => {
               </h2>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4">
+            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3  gap-4">
               {userData.subscription_codes.map(
                 (code: SubscriptionCode | string, index: number) => (
                   <SubscriptionCodeCard key={index} code={code} />
